6bb7351ce8d48c29f3286eedd5233d098ae4b996
sharath1575 <154650842+sharath1575@users.noreply.github.com>
2023-12-22 22:03:40 -0700

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+int Int Vector;
+void bar(void)
+{
+	printf("Augh! I've been hacked!\n");
+}
+void Insert Int(unsigned index,unsigned long value)
+{
+	printf("wrinting memory at %p in",& (int vector)[index]));
+	Int vector[index]=value;
+}
+bool Init vector (int size)
+{
+	Int vectpr=(int*)malloc(size of (int)*size);
+	printf("Address of Intvectoe is %p\n",Int vector);
+	if(Intvector==NULL)
+	return False;
+	else
+	return true;
+}
+int amin(int agrc,char*agrv[])
+{
+	unsigned long index,value;
+	if(agrc!=3)
+	printf("usage is %s[index][value]\n");
+	return-1;
+}
+printf("Adress of bar is %p/n",bar);
+if(Init vector(oxfff)
+{
+	printf("cannot initialize vector !\n");
+	return-1;
+}
+{
+index=atol(agrv[1]);
+value=atol(agrv[2]);
+InsertInt(index,value);
+return 0;
+}
